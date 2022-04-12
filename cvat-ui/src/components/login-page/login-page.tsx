209780c14b9f1b549f28a45df81b02b7d9872217
MilanLudik <milanludik@gmail.com>
2022-04-12 10:35:46 +0200

    Responsivness

--- cvat-ui/src/components/login-page/login-page.tsx
+++ cvat-ui/src/components/login-page/login-page.tsx
@@ -34,53 +34,66 @@ function LoginPageComponent(props: LoginPageComponentProps & RouteComponentProps
             <Header style={{ backgroundColor: 'white' }}>
                 <PictureWrapper src={satcen} style={{ height: 45, paddingLeft: 200 }} />
             </Header>
-            <Content className='background' style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                <Row justify='center' align='middle'>
-                    <Card
-                        style={{
-                            padding: 10,
-                            width: 600,
-                            height: 630,
-                            borderRadius: 15,
-                            boxShadow: '0px 1px 3px rgba(0,1,1,0.1)',
-                            display: 'flex',
-                            justifyContent: 'center',
-                        }}
-                    >
-                        <Col
-                            style={{
-                                display: 'flex',
-                                flexDirection: 'column',
-                                alignItems: 'center',
-                                height: 570,
-                                width: 450,
-                            }}
-                        >
-                            <PictureWrapper src={croas} style={{ height: 200, marginBottom: 30 }} />
-
-                            <LoginForm
-                                fetching={fetching}
-                                onSubmit={(loginData: LoginData): void => {
-                                    onLogin(loginData.username, loginData.password);
+            <div style={{ height: '100%' }}>
+                <Content
+                    className='background'
+                    style={{
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        height: '100%',
+                        padding: 30,
+                    }}
+                >
+                    <Row justify='center' align='middle'>
+                        <div>
+                            <Card
+                                style={{
+                                    padding: 10,
+                                    width: 600,
+                                    height: 630,
+                                    borderRadius: 15,
+                                    boxShadow: '0px 1px 3px rgba(0,1,1,0.1)',
+                                    display: 'flex',
+                                    justifyContent: 'center',
                                 }}
-                            />
+                            >
+                                <Col
+                                    style={{
+                                        display: 'flex',
+                                        flexDirection: 'column',
+                                        alignItems: 'center',
+                                        height: 570,
+                                        width: 450,
+                                    }}
+                                >
+                                    <PictureWrapper src={croas} style={{ height: 200, marginBottom: 30 }} />
 
-                            {renderResetPassword && (
-                                <Row justify='start' align='top'>
-                                    <Col>
-                                        <Text strong>
-                                            <Link to='/auth/password/reset' style={{ color: '#1a3765' }}>
-                                                Forgot password?
-                                            </Link>
-                                        </Text>
-                                    </Col>
-                                </Row>
-                            )}
-                            <PictureWrapper src={disclaimer} style={{ paddingTop: 40 }} />
-                        </Col>
-                    </Card>
-                </Row>
-            </Content>
+                                    <LoginForm
+                                        fetching={fetching}
+                                        onSubmit={(loginData: LoginData): void => {
+                                            onLogin(loginData.username, loginData.password);
+                                        }}
+                                    />
+
+                                    {renderResetPassword && (
+                                        <Row justify='start' align='top'>
+                                            <Col>
+                                                <Text strong>
+                                                    <Link to='/auth/password/reset' style={{ color: '#1a3765' }}>
+                                                        Forgot password?
+                                                    </Link>
+                                                </Text>
+                                            </Col>
+                                        </Row>
+                                    )}
+                                    <PictureWrapper src={disclaimer} style={{ paddingTop: 40 }} />
+                                </Col>
+                            </Card>
+                        </div>
+                    </Row>
+                </Content>
+            </div>
             <Footer
                 style={{
                     justifyContent: 'center',
